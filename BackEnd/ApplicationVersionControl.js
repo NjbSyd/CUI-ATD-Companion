1fbff7e3ae8946d058ef8449eb4abd8ee5c2afb1
Najeeb Said <tcanjb@gmail.com>
2023-09-05 15:31:15 +0500

    update check --update

--- BackEnd/ApplicationVersionControl.js
+++ BackEnd/ApplicationVersionControl.js
@@ -4,7 +4,7 @@ function getApplicationVersion() {
   return Application.nativeApplicationVersion;
 }
 
-// This function is used to check if the app is up-to-date or not.
+// This function is used to check if the app is up-to-datkjsdhfdse or not.
 function checkAppVersion() {
   const appVersion = getApplicationVersion();
   const currentVersion = appVersion.split(".");
