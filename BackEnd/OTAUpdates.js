676f0e15066298c003f75bf1723dcc781747634f
Najeeb Said <tcanjb@gmail.com>
2023-10-18 13:23:24 +0500

    blank Screen issue resolved --update

--- BackEnd/OTAUpdates.js
+++ BackEnd/OTAUpdates.js
@@ -1,5 +1,4 @@
 import * as Updates from "expo-updates";
-import { Alert } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export function updateApp() {
@@ -7,11 +6,9 @@ export function updateApp() {
     Updates.checkForUpdateAsync().then((update) => {
       if (update.isAvailable) {
         Updates.fetchUpdateAsync().then(() => {
-          setTimeout(() => {
-            Updates.reloadAsync().then((r) => console.log(r));
-          }, 3000);
-          AsyncStorage.removeItem("lastSyncDate").then(() => null);
-          alert("Update \nApp is updated. Restarting in 3 seconds.");
+          alert(
+            "Update \nApp is updated. Please restart the app to see changes"
+          );
         });
       }
     });
